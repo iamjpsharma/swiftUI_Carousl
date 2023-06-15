c24df81912a440f0082e594a08c5d37e014f8039
iamjpsharma <sjaiprakash457@gmail.com>
2023-06-16 00:55:02 +0530

    Initial Commit

---
+++ SwiftUI-Carousl/SwiftUI_CarouslApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CarouslApp.swift
+//  SwiftUI-Carousl
+//
+//  Created by iamjpsharma on 15/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CarouslApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
